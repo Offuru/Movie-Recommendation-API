e4e44e71ba99ce5282dba921b8f7ee22eae50332
Offuru <andrei.rozmarin@gmail.com>
2024-06-17 19:29:30 +0300

    Added users controller and placeholder classes for users services and user request related dtos

---
+++ Movie Recommendation API/Movie Recommendation API/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Recommendation_API.Controllers
+{
+    [ApiController]
+    public class BaseController : Controller
+    {
+        public BaseController() { }
+    }
+}
---
+++ Movie Recommendation API/Movie Recommendation API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core.Services;
+using Database.Dtos.Request;
+
+
+namespace Movie_Recommendation_API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : BaseController
+    {
+        private UsersService UsersService { get; set; }
+
+        public UsersController(UsersService usersService)
+        {
+            UsersService = usersService;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add([FromBody] AddUserRequest payload)
+        {
+            return Ok("User has been succesfully created");
+        }
+
+        [HttpGet]
+        [Route("{userId}/get-details")]
+        public IActionResult GetUserDetails([FromRoute] int userId)
+        {
+            return Ok();
+        }
+    }
+}
